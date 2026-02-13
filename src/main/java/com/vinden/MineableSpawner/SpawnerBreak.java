5b49fcf3ca1981e21344c323723dba8280af89d7
morfoloo <morfoloo01@gmail.com>
2026-02-13 18:40:48 +0700

    Fixed spawner drop

--- src/main/java/com/vinden/MineableSpawner/SpawnerBreak.java
+++ src/main/java/com/vinden/MineableSpawner/SpawnerBreak.java
@@ -3,7 +3,6 @@ package com.vinden.MineableSpawner;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.CreatureSpawner;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,7 +12,6 @@ import org.bukkit.inventory.meta.BlockStateMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SpawnerBreak implements Listener {
-
     private final JavaPlugin plugin;
 
     public SpawnerBreak(JavaPlugin plugin) {
@@ -23,37 +21,35 @@ public class SpawnerBreak implements Listener {
     @EventHandler
     public void onSpawnerBreak(BlockBreakEvent event) {
         Block block = event.getBlock();
-        if (block.getType() != Material.SPAWNER) return;
         Player player = event.getPlayer();
-        ItemStack tool = player.getInventory().getItemInMainHand();
-        if (!tool.containsEnchantment(Enchantment.SILK_TOUCH)) {
-            return;
-        }
 
-        event.setDropItems(false); // отменяем стандартный дроп
-        CreatureSpawner spawner = (CreatureSpawner) block.getState();
-        ItemStack spawnerItem = new ItemStack(Material.SPAWNER);
-        BlockStateMeta meta = (BlockStateMeta) spawnerItem.getItemMeta();
-        if (meta != null) {
-            ((CreatureSpawner) meta.getBlockState()).setSpawnedType(spawner.getSpawnedType());
-            spawnerItem.setItemMeta(meta);
+        if (block.getType() == Material.SPAWNER) {
+            event.setDropItems(false);
+            CreatureSpawner spawner = (CreatureSpawner) block.getState();
+            ItemStack spawnerItem = new ItemStack(Material.SPAWNER);
+            BlockStateMeta meta = (BlockStateMeta) spawnerItem.getItemMeta();
+            if (meta != null && spawner.getSpawnedType() != null) {
+                ((CreatureSpawner) meta.getBlockState()).setSpawnedType(spawner.getSpawnedType());
+                spawnerItem.setItemMeta(meta);
+            }
+            block.getWorld().dropItemNaturally(block.getLocation(), spawnerItem);
+            if (spawner.getSpawnedType() != null) {
+                Material spawnEgg;
+                switch (spawner.getSpawnedType()) {
+                    case ZOMBIE -> spawnEgg = Material.ZOMBIE_SPAWN_EGG;
+                    case SKELETON -> spawnEgg = Material.SKELETON_SPAWN_EGG;
+                    case SPIDER -> spawnEgg = Material.SPIDER_SPAWN_EGG;
+                    case CAVE_SPIDER -> spawnEgg = Material.CAVE_SPIDER_SPAWN_EGG;
+                    case SILVERFISH -> spawnEgg = Material.SILVERFISH_SPAWN_EGG;
+                    case BLAZE -> spawnEgg = Material.BLAZE_SPAWN_EGG;
+                    case MAGMA_CUBE -> spawnEgg = Material.MAGMA_CUBE_SPAWN_EGG;
+                    default -> spawnEgg = null;
+                }
+                if (spawnEgg != null) {
+                    block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(spawnEgg));
+                }
+            }
+            block.setType(Material.AIR);
         }
-
-        Material spawnEgg = switch (spawner.getSpawnedType()) {
-            case ZOMBIE -> Material.ZOMBIE_SPAWN_EGG;
-            case SKELETON -> Material.SKELETON_SPAWN_EGG;
-            case SPIDER -> Material.SPIDER_SPAWN_EGG;
-            case CAVE_SPIDER -> Material.CAVE_SPIDER_SPAWN_EGG;
-            case SILVERFISH -> Material.SILVERFISH_SPAWN_EGG;
-            case BLAZE -> Material.BLAZE_SPAWN_EGG;
-            case MAGMA_CUBE -> Material.MAGMA_CUBE_SPAWN_EGG;
-            default -> null;
-        };
-
-        block.getWorld().dropItemNaturally(block.getLocation(), spawnerItem);
-        if (spawnEgg != null) {
-            block.getWorld().dropItemNaturally(block.getLocation(), new ItemStack(spawnEgg));
-        }
-        block.setType(Material.AIR);
     }
 }
